cf20629d7d6fde8ff8d18a33034c459d08c44f45
Rob Everhardt <rob@everhardt.nl>
2020-05-20 10:08:50 +0200

    chore: drop default from fieldConfigurations

--- src/models/field-configuration.ts
+++ src/models/field-configuration.ts
@@ -108,30 +108,21 @@ const schema = Joi.object().keys({
   key: Joi.string().pattern(/^[a-z][a-zA-Z\d]*$/).required().example('id'),
   name: Joi.string().required().example('ID as used in our geographic information system'),
   inputType: Joi.valid('text', 'textarea', 'select', 'radio', 'switch', 'checkbox', 'file', 'files')
-    .description('The UI component to show. When \'file\' or \'files\' is selected, it is not passed through parser, default is ignored.'),
+    .description('The UI component to show. When \'file\' or \'files\' is selected, it is not passed through parser.'),
   valueOptions: Joi.array().items(Joi.object().keys({
     text: Joi.string().required(),
     value: baseFieldSchema.required().description('Will be passed through parser'),
   })).allow(null)
     .description('If null, inputType should not be select or radio. If not null, input type should be select or radio'),
-  default: Joi.alternatives().try(
-    Joi.object().keys({
-      type: Joi.string().valid('udf').required().description('Determines how default.value should be interpreted: as a constant value (\'value\'), or as a user defined function (\'udf\').'),
-      value: Joi.string().required().description('Will not be passed through parser, but immediately to validator. Valid javascript function with signature ({ values: Record<string, boolean | number | string | null | FileToFieldConfigurationUdf | FileToFieldConfigurationUdf[]>, additionalData: Record<string, any>}) => boolean | number | string | null'),
-    }),
-    Joi.object().keys({
-      type: Joi.string().valid('value').required().description('Determines how default.value should be interpreted: as a constant value (\'value\'), or as a user defined function (\'udf\').'),
-      value: baseFieldSchema.required().description('Will not be passed through parser, but immediately to validator.'),
-    }),
-  ).description('Not available for inputTypes \'file\' or \'files\''),
   parser: Joi.string().description('Parses the input string. Valid javascript function with signature ({ raw: Record<string, string>, object: [tbd]) => boolean | number | string | null'),
   validator: Joi.string().description('Validates the parsed input. Valid javascript function with signature ({ parsed: string, boolean | number | string | null | FileToFieldConfigurationUdf | FileToFieldConfigurationUdf[], values: Record<string, boolean | number | string | null | FileToFieldConfigurationUdf  | FileToFieldConfigurationUdf[]>, additionalData: Record<string,any>}) => true | string. When true is returned, input is valid. When a string is returned, input is invalid and the string is shown as error.'),
   show: Joi.string().description('Determines whether this field should be shown. Valid javascript function with signature (value: Record<string, { value: boolean | number | string | null | FileToFieldConfigurationUdf  | FileToFieldConfigurationUdf[], validationResult: true | string }, object: [tbd]) => \'always\' | \'never\' | \'optional\'. Optional fields are hidden when they have no data'),
   header: Joi.string().description('Adds a header before this field (only if this field is shown). Valid javascript function with signature (value: Record<string, { value: boolean | number | string | null | FileToFieldConfigurationUdf | FileToFieldConfigurationUdf[], validationResult: true | string }, object: [tbd]) => string | false'),
-  unparser: Joi.string().description('Converts the stored value back to what should be shown in the input field. Valid javascript function with signature (value: Record<string, { value: boolean | number | string | null | FileToFieldConfigurationUdf | FileToFieldConfigurationUdf[] }, object: [tbd]) => string'),
+  unparser: Joi.string().description('Converts the stored value back to what should be shown in the input field. Is also an opportunity to provide a default value. Valid javascript function with signature (value: Record<string, { value: boolean | number | string | null | FileToFieldConfigurationUdf | FileToFieldConfigurationUdf[] }, object: [tbd]) => string'),
   prefix: Joi.string().description('Not available for inputTypes \'radio\', \'switch\', \'checkbox\', \'file\' and \'files\''),
   suffix: Joi.string().description('Not available for inputTypes \'radio\', \'switch\', \'checkbox\', \'file\' and \'files\''),
   hint: Joi.string().allow('').description('As shown near the input field'),
+  store: Joi.boolean().default(true).description('If false, the field will not be stored. This is useful to make more elaborate UI\'s where for example a (non-stored) switch determines which other fields to show'),
 })
   .tag('fieldConfiguration')
   .description('Defines which data can be stored on objects of a certain type. See the open fields chapter for more detail.');
@@ -146,7 +137,6 @@ interface FieldConfiguration {
   name: string;
   inputType?: 'text' | 'textarea' | 'select' | 'radio' | 'switch' | 'checkbox' | 'file' | 'files';
   valueOptions?: ValueOption[] | null;
-  default?: { type: 'udf'; value: string } | { type: 'value'; value: BaseField };
   parser?: string;
   validator?: string;
   show?: string;
@@ -155,6 +145,7 @@ interface FieldConfiguration {
   prefix?: string;
   suffix?: string;
   hint?: string;
+  store: boolean;
 }
 
 interface UpdatableFieldConfiguration {
