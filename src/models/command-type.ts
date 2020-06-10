41ad709a4630c8d82a2354f107b2f3ed1f5da641
Rob Everhardt <rob@everhardt.nl>
2020-06-10 14:57:45 +0200

    fix: commandType.start and .end values should be limited

--- src/models/command-type.ts
+++ src/models/command-type.ts
@@ -6,9 +6,9 @@ import { fieldConfigurationFromServerSchema, FieldConfigurationFromServer } from
 const schema = Joi.object().keys({
   hashId: Joi.string().required().example('x18a92'),
   name: Joi.string().required().example('Measurement cycle'),
-  start: Joi.string().allow('required', 'optional', 'disabled').required().example('required')
+  start: Joi.string().valid('required', 'optional', 'disabled').required().example('required')
     .description('\'required\': user must provide command.startAt. \'optional\': user can provide command.startAt or a delay for the command to start after it is sent to the device. \'disabled\': user cannot provide command.startAt nor a delay.'),
-  end: Joi.string().allow('required', 'optional', 'disabled').required().example('disabled')
+  end: Joi.string().valid('required', 'optional', 'disabled').required().example('disabled')
     .description('\'required\': user must provide command.endAt. \'optional\': user can provide command.endAt. \'disabled\': user cannot provide command.endAt.'),
   supplierOnly: Joi.boolean().required().example(false).description('If true, this type of command can not be created from an environment'),
   fieldConfigurations: Joi.array().items(fieldConfigurationFromServerSchema).required()
--- src/routes/command-type/add.ts
+++ src/routes/command-type/add.ts
@@ -27,8 +27,8 @@ const controllerGeneratorOptions: ControllerGeneratorOptions = {
   path: '/',
   body: Joi.object().keys({
     name: Joi.string().required().example('Measurement cycle'),
-    start: Joi.allow('required', 'optional', 'disabled').default('optional').description('\'required\': user must provide command.startAt. \'optional\': user can provide command.startAt or a delay for the command to start after it is sent to the device. \'disabled\': user cannot provide command.startAt nor a delay.'),
-    end: Joi.allow('required', 'optional', 'disabled').default('disabled').description('\'required\': user must provide command.endAt. \'optional\': user can provide command.endAt. \'disabled\': user cannot provide command.endAt.'),
+    start: Joi.string().valid('required', 'optional', 'disabled').default('optional').description('\'required\': user must provide command.startAt. \'optional\': user can provide command.startAt or a delay for the command to start after it is sent to the device. \'disabled\': user cannot provide command.startAt nor a delay.'),
+    end: Joi.string().valid('required', 'optional', 'disabled').default('disabled').description('\'required\': user must provide command.endAt. \'optional\': user can provide command.endAt. \'disabled\': user cannot provide command.endAt.'),
     supplierOnly: Joi.boolean().default(false).description('If true, this type of command can not be created from an environment'),
     fieldConfigurations: Joi.array().items(fieldConfigurationToServerSchema).required()
       .description('See the chapter on open fields on how to use this'),
