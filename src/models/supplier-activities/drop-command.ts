752388f039f4efab4ec74ba0f0b113feec81b592
Rob Everhardt <rob@everhardt.nl>
2020-09-08 12:49:55 +0200

    feat: extend exec.dropCommand with parameters for removal from scheduledCommands and for deletion
    contributes to https://github.com/withthegrid/platform/issues/476

--- src/models/supplier-activities/drop-command.ts
+++ src/models/supplier-activities/drop-command.ts
@@ -5,21 +5,22 @@ import { schema as commandSchema, Command } from '../command';
 import { schema as commandTypeSchema, CommandType } from '../command-type';
 
 interface DropCommand extends SupplierActivity<'dropCommand'> {
-  triggerData: Record<string, never> | {
-    command: Command;
-    commandType: CommandType;
+  triggerData: {
+    command?: Command;
+    commandType?: CommandType;
+    removeFromScheduledCommands: boolean;
+    markAsDeleted: boolean;
   };
 }
 
 const schema = supplierActivityConstructor(
   'dropCommand',
-  Joi.alternatives().try(
-    Joi.object().keys({
-      command: commandSchema.required(),
-      commandType: commandTypeSchema.required(),
-    }).required(),
-    Joi.object().keys({}).required(),
-  ).required(),
+  Joi.object().keys({
+    command: commandSchema,
+    commandType: commandTypeSchema,
+    removeFromScheduledCommands: Joi.boolean().required().example(true),
+    markAsDeleted: Joi.boolean().required().example(false),
+  }).required(),
 )
   .tag('supplierActivityDropCommand')
   .description('User defined function in the connectivity environment that informs the system that a specific command can be removed from the list of scheduled commands. This can be used, for example, as it was sent to the device and is not needed anymore, or when its scheduled execution time has passed.');
