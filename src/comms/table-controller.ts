7e07b2bbae4825430161c36518f7c447b4181218
Rob Everhardt <rob@everhardt.nl>
2020-10-08 13:36:27 +0200

    feat: table controller with flexible maxRowsPerPage and easier paging

--- src/comms/table-controller.ts
+++ src/comms/table-controller.ts
@@ -2,8 +2,18 @@ import Joi from 'joi';
 
 import { Result, RequestQuery } from './controller';
 
+/**
+ *
+ * Table controllers support two ways of communicating offset with the server:
+ * - lastValueSortColumn/ lastValueHashId: this sdk takes care of mapping the
+ *   the page to these two values to retrieve the next page
+ * - offset: for routes where the server supplies nextPageOffset on each page,
+ *   that can be used to request the next page
+ */
+
 function tableQuerySchemaGenerator(
   sortBy: Joi.StringSchema = Joi.string().valid('hashId').default('hashId'),
+  maxRowsPerPage = 100,
 ): Joi.ObjectSchema {
   return Joi.object().keys({
     sortBy,
@@ -11,11 +21,12 @@ function tableQuerySchemaGenerator(
     rowsPerPage: Joi.number()
       .integer()
       .min(1)
-      .max(100)
+      .max(maxRowsPerPage)
       .default(10),
     search: Joi.string().allow('').default(''),
     lastValueSortColumn: Joi.any().description('To retrieve the next page, provide lastValueSortColumn and lastValueHashId. lastValueSortColumn is the value for the key provided in sortBy of the last row that is received. Should also be provided when sortBy is \'hashId\''),
     lastValueHashId: Joi.string().description('To retrieve the next page, provide lastValueSortColumn and lastValueHashId. lastValueHashId is the hashId of the last row that is received.'),
+    offset: Joi.string().description('To retrieve the next page, provide of the nextPageOffset of the last page when available and not null (lastValueSortColumn and lastValueHashId can be ignored then).'),
     updatedAfter: Joi.date().iso().description('Only rows that are updated after the provided date will be returned'),
   }).with('lastValueSortColumn', 'lastValueHashId').default();
 }
@@ -30,6 +41,7 @@ interface DefaultedTableQueryParameters extends RequestQuery {
 interface TableQuery extends DefaultedTableQueryParameters {
   lastValueSortColumn?: string | number | Date | null;
   lastValueHashId?: string;
+  offset?: string;
   updatedAfter?: Date;
 }
 
@@ -37,11 +49,7 @@ interface TableRequest {
   query: TableQuery;
 }
 
-interface EffectiveTableQuery extends Required<DefaultedTableQueryParameters> {
-  lastValueSortColumn?: string | number | Date | null;
-  lastValueHashId?: string;
-  updatedAfter?: Date;
-}
+type EffectiveTableQuery = Required<DefaultedTableQueryParameters> & TableQuery;
 
 interface EffectiveTableRequest {
   query: EffectiveTableQuery;
@@ -52,20 +60,27 @@ interface ObjectKeyResult {
   lastValueHashId: string;
 }
 
+interface Response<RowImplementation> {
+  rows: RowImplementation[];
+  nextPageOffset?: string | null;
+}
+
 class TableController<RowImplementation> {
   pagesAcquired: number;
 
   rows: RowImplementation[];
 
-  result: Result<EffectiveTableRequest, { rows: RowImplementation[] }> | null;
+  result: Result<EffectiveTableRequest, Response<RowImplementation>> | null;
 
   lastValueSortColumn?: string | number | Date | null;
 
   lastValueHashId?: string;
 
+  nextPageOffset?: string | null;
+
   constructor(
-    private readonly route: (parameters?: TableRequest) => Result<EffectiveTableRequest, { rows: RowImplementation[] }>, // eslint-disable-line max-len
-    readonly objectKeyMapper: (row: RowImplementation, sortBy: string) => ObjectKeyResult,
+    private readonly route: (parameters?: TableRequest) => Result<EffectiveTableRequest, Response<RowImplementation>>, // eslint-disable-line max-len
+    readonly objectKeyMapper?: (row: RowImplementation, sortBy: string) => ObjectKeyResult,
     readonly parameters?: TableQuery,
   ) {
     this.pagesAcquired = 0;
@@ -83,26 +98,49 @@ class TableController<RowImplementation> {
 
     params.lastValueSortColumn = this.lastValueSortColumn;
     params.lastValueHashId = this.lastValueHashId;
+    if (this.nextPageOffset === null) {
+      throw new Error('Has reached the last page');
+    }
+    params.offset = this.nextPageOffset;
 
     const result = this.route({ query: params });
     this.result = result;
 
     const response = await result.response;
 
-    if (response.rows.length > 0) {
-      const lastRow = response.rows[response.rows.length - 1];
-      const { lastValueSortColumn, lastValueHashId } = this.objectKeyMapper(
-        lastRow,
-        result.request.query.sortBy,
-      );
+    this.add(response, result.request.query.sortBy);
+    return this.pagesAcquired;
+  }
 
-      this.lastValueSortColumn = lastValueSortColumn;
-      this.lastValueHashId = lastValueHashId;
+  /**
+   * No diffing or sorting is done, so should only be called to add new pages on
+   * top of the already stored ones. Can be used by an implementation if it has
+   * retrieved rows by another means (eg. by a manual call to a .find
+   * controller)
+   *
+   * Be sure to add only one page at a time and ensure it is the complete result
+   * for that page, otherwise this.pagesAcquired gets fucked up
+   */
+  add(response: Response<RowImplementation>, sortBy: string): void {
+    if (response.rows.length > 0) {
+      if (response.nextPageOffset !== undefined) {
+        this.nextPageOffset = response.nextPageOffset;
+      } else if (this.objectKeyMapper !== undefined) {
+        const lastRow = response.rows[response.rows.length - 1];
+        const { lastValueSortColumn, lastValueHashId } = this.objectKeyMapper(
+          lastRow,
+          sortBy,
+        );
+
+        this.lastValueSortColumn = lastValueSortColumn;
+        this.lastValueHashId = lastValueHashId;
+      } else {
+        throw new Error('Response has no nextPageOffset and objectKeyMapper is not defined');
+      }
 
       this.rows = this.rows.concat(response.rows);
     }
     this.pagesAcquired += 1;
-    return this.pagesAcquired;
   }
 
   get(page: number): RowImplementation[] {
