ea8047c69e78901bab3abd5b4dbb4b57feac8429
Rob Everhardt <rob@everhardt.nl>
2020-10-08 13:36:51 +0200

    chore: drop redundant objects key in autocomplete router

--- src/routes/autocomplete/find.ts
+++ src/routes/autocomplete/find.ts
@@ -24,12 +24,6 @@ const controllerGeneratorOptions: ControllerGeneratorOptions = {
   query: Joi.object().keys({
     search: Joi.string().required().allow('').example('a')
       .description('Only objects whose name start with the provided string are returned. For devices, it is matched against its hashId'),
-    objects: Joi.array().items(Joi.object().keys({
-      type: Joi.string().valid('device', 'pinGroup', 'edge', 'grid').required().example('device'),
-      filter: Joi.array().items(Joi.string()).default([]).description('only for devices, filters the result on device type'),
-      linkedOnly: Joi.boolean().default(false).description('only for device and pinGroup types'),
-      unlinkedOnly: Joi.boolean().default(false).description('only for device and pinGroup types'),
-    })),
   }).required(),
   right: { environment: 'READ' },
   description: 'Get up to 10 objects of your choice matching the provided string',
