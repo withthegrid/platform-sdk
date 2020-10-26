0e830f1ce985a488f0057613bb8c33e1d5f17027
Rob Everhardt <rob@everhardt.nl>
2020-10-26 20:38:59 +0100

    feat: return pinGrops and lastReports in graph.updateGrid

--- src/routes/graph/update-grid.ts
+++ src/routes/graph/update-grid.ts
@@ -3,6 +3,7 @@ import { ControllerGeneratorOptions } from '../../comms/controller';
 import { schema as fieldsToServerUpdateSchema, FieldsToServerUpdate } from '../../models/fields/fields-to-server-update';
 
 import { schema as gridSchema, Grid } from '../../models/grid';
+import { schema as pinGroupSchema, PinGroup } from '../../models/pin-group';
 
 interface Request {
   params: {
@@ -17,6 +18,11 @@ interface Request {
 
 interface Response {
   grid: Grid;
+  pinGroups: PinGroup[];
+  lastReports: {
+    pinGroupHashId: string;
+    generatedAt: Date;
+  }[];
 }
 
 const controllerGeneratorOptions: ControllerGeneratorOptions = {
@@ -30,8 +36,13 @@ const controllerGeneratorOptions: ControllerGeneratorOptions = {
     photo: Joi.string().allow(null).description('Should be a dataurl. Null clears the photo'),
     pinGroupHashIds: Joi.array().items(Joi.string()).description('Determines the set (and the order) of the pin groups in the grid'),
   }).required(),
-  response: Joi.object().keys({
+  response: (apiVersion: number): Joi.ObjectSchema => Joi.object().keys({
     grid: gridSchema.required(),
+    pinGroups: Joi.array().items(pinGroupSchema(apiVersion)).required(),
+    lastReports: Joi.array().items(Joi.object({
+      pinGroupHashId: Joi.string().required().example('dao97'),
+      generatedAt: Joi.date().required().example('2019-12-31T15:23Z'),
+    })).required(),
   }).required(),
   right: { environment: 'STATIC' },
   description: 'Updates a specific grid',
