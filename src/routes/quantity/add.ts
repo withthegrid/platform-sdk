88e9e0f6e2af2249d0b3bc1e98dcd20f468553cf
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2021-01-08 12:12:13 +0200

    feat: add default order of magnitude into quantity interfaces and schema
    replaced required() with default(0) for quantity.add and quantity.update as well

--- src/routes/quantity/add.ts
+++ src/routes/quantity/add.ts
@@ -20,7 +20,7 @@ const controllerGeneratorOptions: ControllerGeneratorOptions = {
     name: Joi.string().required().example('Temperature'),
     unit: Joi.string().required().example('K').description('Will be displayed with an SI-prefix (eg. k or M) if relevant'),
     defaultOrderOfMagnitude: Joi.number().integer().min(-128).max(127)
-      .required()
+      .default(0)
       .example(3)
       .description('Defines default order of magnitude to be selected at manual report form'),
   }).required(),
