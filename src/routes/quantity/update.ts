34cb6d40ecc08a052df9fdde494749db6a71ff8f
Rob Everhardt <rob@everhardt.nl>
2021-01-08 12:03:51 +0100

    fix: defaultOrderOfMagnitude should not have a default in quantity.update
    As the parameter is optional. If a default is provided, it is no longer optional

--- src/routes/quantity/update.ts
+++ src/routes/quantity/update.ts
@@ -24,7 +24,6 @@ const controllerGeneratorOptions: ControllerGeneratorOptions = {
     name: Joi.string().example('Temperature'),
     unit: Joi.string().example('K').description('Will be displayed with an SI-prefix (eg. k or M) if relevant'),
     defaultOrderOfMagnitude: Joi.number().integer().min(-128).max(127)
-      .default(0)
       .example(3)
       .description('Defines default order of magnitude to be selected at manual report form'),
   }).required(),
