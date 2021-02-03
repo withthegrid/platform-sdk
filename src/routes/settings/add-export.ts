b44a50c964666219c506594718da28ec98332ff5
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2021-02-03 22:23:56 +0200

    feat: create export request by condition overview hash id
    started using own definition of body.content property at routes/settings/add-export.ts,
    without name and description in schema.

--- src/routes/settings/add-export.ts
+++ src/routes/settings/add-export.ts
@@ -42,7 +42,27 @@ const controllerGeneratorOptions: ControllerGeneratorOptions = {
         type: Joi.string().required().valid('measurementFilter').example('measurementFilter'),
         measurementFilterHashId: Joi.string().required(),
       }).required(),
-      ...contentSchemaAlternatives,
+      Joi.object().keys({
+        type: Joi.string().required().valid('all').example('all'),
+        staticOnly: Joi.boolean().required().example(false),
+        gridHashId: Joi.string().allow(null).required().example(null),
+        from: Joi.date().iso().required().example('2019-12-01T00:00Z'),
+        to: Joi.date().iso().required().example('2020-01-01T00:00Z'),
+      }).required(),
+      Joi.object().keys({
+        type: Joi.string().required().valid('measurementFilter').example('measurementFilter'),
+        includePinsWithoutReports: Joi.boolean().required().example(true),
+        reportTypeHashIds: Joi.array().min(1).max(20).items(Joi.string().example('l19a7s'))
+          .default([]),
+        gridHashId: Joi.string().allow(null).required(),
+        pinGroupHashIds: Joi.array().min(0).max(50).items(Joi.string().example('dao97'))
+          .required(),
+        quantityHashIds: Joi.array().max(20).items(Joi.string().example('sajia1'))
+          .default([]),
+        fieldKeys: Joi.array().max(20).items(Joi.string().example('id')).default([]),
+        from: Joi.date().iso().example('2019-12-01T00:00Z'),
+        to: Joi.date().iso().example('2020-01-01T00:00Z'),
+      }).required(),
     ).required(),
     delimiter: Joi.string().valid(',', ';').required().example(','),
     rowDelimiter: Joi.string().valid('\n', '\r\n').required().example('\n'),
