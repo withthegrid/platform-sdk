d871e5bd405faff8de458a369aa352fda3b55f13
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2021-03-05 10:32:54 +0200

    feat(graph/set-threshold): request body can be array (#22)
    * feat(graph/set-threshold): request body can be array
    
    * chore: descriptions updated

--- src/routes/graph/set-threshold.ts
+++ src/routes/graph/set-threshold.ts
@@ -3,40 +3,53 @@ import { ControllerGeneratorOptions } from '../../comms/controller';
 
 import { schema as siNumberSchema, SiNumber } from '../../models/si-number';
 
+interface Body {
+  pinHashId: string;
+  quantityHashId: string;
+  thresholds: {
+    criticallyLow: SiNumber | null;
+    low: SiNumber | null;
+    high: SiNumber | null;
+    criticallyHigh: SiNumber | null;
+  } | null;
+}
+
 interface Request {
-  body: {
-    pinHashId: string;
-    quantityHashId: string;
-    thresholds: {
-      criticallyLow: SiNumber | null;
-      low: SiNumber | null;
-      high: SiNumber | null;
-      criticallyHigh: SiNumber | null;
-    } | null;
-  };
+  body: Body | Body[];
 }
 
 interface Response {
-  hashId?: string;
+  hashId?: string | string[];
 }
+
+const bodySchema = Joi.object().keys({
+  pinHashId: Joi.string().required().example('e13d57'),
+  quantityHashId: Joi.string().required().example('sajia1'),
+  thresholds: Joi.object().keys({
+    criticallyLow: siNumberSchema.allow(null).required().description('If null, incoming measurements are not checked against this value'),
+    low: siNumberSchema.allow(null).required().description('If null, incoming measurements are not checked against this value'),
+    high: siNumberSchema.allow(null).required().description('If null, incoming measurements are not checked against this value'),
+    criticallyHigh: siNumberSchema.allow(null).required().description('If null, incoming measurements are not checked against this value'),
+  }).allow(null).required(),
+}).required();
 
 const controllerGeneratorOptions: ControllerGeneratorOptions = {
   method: 'post',
   path: '/thresholds',
-  body: Joi.object().keys({
-    pinHashId: Joi.string().required().example('e13d57'),
-    quantityHashId: Joi.string().required().example('sajia1'),
-    thresholds: Joi.object().keys({
-      criticallyLow: siNumberSchema.allow(null).required().description('If null, incoming measurements are not checked against this value'),
-      low: siNumberSchema.allow(null).required().description('If null, incoming measurements are not checked against this value'),
-      high: siNumberSchema.allow(null).required().description('If null, incoming measurements are not checked against this value'),
-      criticallyHigh: siNumberSchema.allow(null).required().description('If null, incoming measurements are not checked against this value'),
-    }).allow(null).required(),
-  }).required(),
+  body: Joi.alternatives().try(
+    bodySchema,
+    Joi.array().items(bodySchema).required(),
+  ).required(),
   right: { environment: 'ISSUES' },
   response: Joi.object().keys({
-    hashId: Joi.string().example('tap192')
-      .description('If thresholds is set to null, this key will not be present'),
+    hashId: Joi.alternatives().try(
+      Joi.string().allow(null).required().example('tap192')
+        .description('If thresholds is set to null, this property will be null'),
+      Joi.array().items(
+        Joi.string().allow(null).required().example('tap192')
+          .description('If thresholds in body array item is set to null, the corresponding hashId item will be null'),
+      ).required(),
+    ).required(),
   }).required(),
   description: 'Sets issue triggers (thresholds) on a specific quantitity on a specific port (pin). If a measurement outside these limits is registered, an issue is automatically created (if there isn\'t a relevant one open yet)',
 };
