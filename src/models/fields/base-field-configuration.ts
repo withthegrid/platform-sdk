c70ffa5ffa9333d15bf70e01d94befd82bca9b9b
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2021-04-29 12:59:47 +0300

    chore: wip

--- src/models/fields/base-field-configuration.ts
+++ src/models/fields/base-field-configuration.ts
@@ -3,7 +3,12 @@ import { schema as baseFieldSchema, BaseField } from './base-field';
 
 const schema = Joi.object().keys({
   key: Joi.string().pattern(/^[a-z][a-zA-Z\d]*$/).required().example('id'),
-  name: Joi.string().required().example('ID as used in our geographic information system'),
+  type: Joi.string().required().valid('string', 'boolean', 'number', 'integer')
+    .default('string'),
+  name: Joi.alternatives(
+    Joi.string().required(),
+    Joi.object()..required(),
+  ),
   inputType: Joi.string().valid('text', 'textarea', 'select', 'radio', 'switch', 'checkbox', 'file', 'files')
     .description('The UI component to show. If not specified, it is text unless valueOptions are provided, then it is select.'),
   valueOptions: Joi.array().items(Joi.object().keys({
@@ -19,18 +24,25 @@ const schema = Joi.object().keys({
   .description('Defines which data can be stored in form fields.');
 
 interface ValueOption {
-  text: string;
+  // string for backwards compatibility, shown at all locales
+  text: string | { [locale: string]: string };
   value: BaseField;
 }
 
 interface BaseFieldConfiguration {
   key: string;
-  name: string;
-  inputType?: 'text' | 'textarea' | 'select' | 'radio' | 'switch' | 'checkbox' | 'file' | 'files';
-  valueOptions?: ValueOption[] | null;
-  prefix?: string;
-  suffix?: string;
-  hint?: string;
+  type: 'string' | 'boolean' | 'number' | 'integer'; // in Joi, set string as default
+  name: string | { [locale: string]: string }; // string for backwards compatibility, shown at all locales
+  inputType: 'text' | 'textarea' | 'select' | 'radio' | 'switch' | 'checkbox' | 'file' | 'files'; // in Joi, set to text by default, unless valueOptions are provided, then set to select
+  defaultValue: string | boolean | number; // in Joi, set default based on type: '' for string, true for boolean, 0 for number and integer
+  valueOptions: ValueOption[] | null;  // in Joi, set to null by default
+  regex?: string; // if provided, type should be string and the provided value should adhere to this regex
+  lowerbound?: number; // if provided, type should be number or integer (and lowerbound should be integer as well in case type is integer) and provided value should not be lower than this value
+  upperbound?: number; // if provided, type should be number or integer (and lowerbound should be integer as well in case type is integer) and provided value should not be higher than this value
+  showIf?: { key: string, value: string | boolean | number }; // key should be of a configuration earlier in the array, otherwise the configuration is not accepted (to avoid circular references)
+  prefix?: string | { [locale: string]: string }; // string for backwards compatibility, shown at all locales
+  suffix?: string | { [locale: string]: string }; // string for backwards compatibility, shown at all locales
+  hint?: string | { [locale: string]: string }; // string for backwards compatibility, shown at all locales
 }
 
 export {
