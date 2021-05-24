2817d163703652d8cfe06ab393bb6a6a0d9ecf6f
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2021-05-24 06:07:53 +0300

    feat: added report late supplier activity

---
+++ src/models/supplier-activities/handle-report-late.ts
@@ -0,0 +1,22 @@
+import Joi from 'joi';
+
+import { schemaConstructor as supplierActivityConstructor, SupplierActivity } from './base';
+
+import { schema as deviceTypeHandlerActivity, DeviceTypeHandlerActivity } from './device-type-handler-activity';
+
+interface HandleReportLate extends SupplierActivity<'handleReportLate'> {
+  triggerData: {
+  };
+  activities: DeviceTypeHandlerActivity[];
+}
+
+const schema = (apiVersion: number): Joi.ObjectSchema => supplierActivityConstructor(
+  'handleReportLate',
+  Joi.object().keys({
+  }).required(),
+  deviceTypeHandlerActivity(apiVersion),
+)
+  .tag('supplierActivityHandleLink')
+  .description('Device type event handler handled the installation of a device at a location.');
+
+export { schema, HandleReportLate };
