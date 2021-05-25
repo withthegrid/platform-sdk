28554e66f460662e94a9c00fa94e412375988d11
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2021-05-25 16:31:25 +0300

    feat: json schema for base field configuration

--- src/models/fields/base-field-configuration.ts
+++ src/models/fields/base-field-configuration.ts
@@ -1,83 +1,13 @@
 import Joi from 'joi';
-import { schema as baseFieldSchema, BaseField } from './base-field';
+import { BaseField } from './base-field';
 import {
   stringOrTranslationsSchema,
   StringOrTranslations,
 } from '../../translations';
 
-const schema = Joi.object().keys({
+const commonBaseFieldConfigurationSchema = Joi.object().keys({
   key: Joi.string().pattern(/^[a-z][a-zA-Z\d]*$/).required().example('id'),
-  type: Joi.string().valid('string', 'boolean', 'number', 'integer')
-    .default('string')
-    .when(Joi.ref('regex'), {
-      is: Joi.string().required(),
-      then: Joi.string().valid('string').default('string'),
-    })
-    .when(Joi.ref('lowerbound'), {
-      is: Joi.number().required(),
-      then: Joi.string().valid('number').default('number'),
-    })
-    .when(Joi.ref('upperbound'), {
-      is: Joi.number().required(),
-      then: Joi.string().valid('number').default('number'),
-    })
-    .when(Joi.ref('inputType'), {
-      is: Joi.string().valid('switch', 'checkbox', 'radio', 'select').required(),
-      then: Joi.string().valid('boolean').default('boolean'),
-    })
-    .when(Joi.ref('inputType'), {
-      is: Joi.string().valid('text', 'textarea').required(),
-      then: Joi.string().valid('string').default('string'),
-    }),
   name: stringOrTranslationsSchema.required(),
-  inputType: Joi
-    .when(Joi.ref('valueOptions'), {
-      is: Joi.array().required(),
-      then: Joi.string()
-        .valid('select', 'radio')
-        .default('select'),
-      otherwise: Joi.string()
-        .valid('text', 'textarea', 'switch', 'checkbox', 'file', 'files')
-        .default('text'),
-    })
-    .description('The UI component to show. If not specified, it is text unless valueOptions are provided, then it is select.'),
-  defaultValue: Joi
-    .when(Joi.ref('type'), {
-      is: 'string',
-      then: Joi.string().allow('').default(''),
-    })
-    .when(Joi.ref('type'), {
-      is: 'number',
-      then: Joi.number().default(0),
-    })
-    .when(Joi.ref('type'), {
-      is: 'integer',
-      then: Joi.number().integer().default(0),
-    })
-    .when(Joi.ref('type'), {
-      is: 'boolean',
-      then: Joi.boolean().default(false),
-    }),
-  valueOptions: Joi.array().items(Joi.object().keys({
-    text: stringOrTranslationsSchema.required(),
-    value: baseFieldSchema.required().description('Will be passed through parser'),
-  })).allow(null).default(null)
-    .description('If null, inputType should not be select or radio. If not null, input type should be select or radio. Default: null'),
-  regex: Joi.string().description('If provided, type should be string and the provided value should adhere to this regex')
-    .when(Joi.ref('type'), {
-      not: 'string',
-      then: Joi.forbidden(),
-    }),
-  lowerbound: Joi.number().description('If provided, type should be number or integer and provided value should not be lower than this value')
-    .when(Joi.ref('type'), {
-      not: ['number', 'integer'],
-      then: Joi.forbidden(),
-    }),
-  upperbound: Joi.number().description('If provided, type should be number or integer and provided value should not be higher than this value')
-    .when(Joi.ref('type'), {
-      not: ['number', 'integer'],
-      then: Joi.forbidden(),
-    }),
   showIf: Joi.object().keys({
     key: Joi.string().required(),
     value: Joi.alternatives(
@@ -86,44 +16,236 @@ const schema = Joi.object().keys({
       Joi.boolean(),
     ).required(),
   }).description('Show this field if other field with provided key is set to provided value. Referenced field must exists already'),
-  prefix: stringOrTranslationsSchema.description('Not available for inputTypes \'radio\', \'switch\', \'checkbox\', \'file\' and \'files\'')
-    .when(Joi.ref('type'), {
-      not: 'string',
-      then: Joi.forbidden(),
-    }),
-  suffix: stringOrTranslationsSchema.description('Not available for inputTypes \'radio\', \'switch\', \'checkbox\', \'file\' and \'files\'')
-    .when(Joi.ref('type'), {
-      not: 'string',
-      then: Joi.forbidden(),
-    }),
+  prefix: stringOrTranslationsSchema.description('Not available for inputTypes \'radio\', \'switch\', \'checkbox\', \'file\' and \'files\''),
+  suffix: stringOrTranslationsSchema.description('Not available for inputTypes \'radio\', \'switch\', \'checkbox\', \'file\' and \'files\''),
   hint: Joi.string().allow('').description('As shown near the input field'),
-})
+});
+
+const getBaseFieldConfigurationSchema = (
+  commonSchema: Joi.ObjectSchema,
+): Joi.AlternativesSchema => Joi.alternatives().try(
+  commonSchema.keys({
+    type: Joi.string().valid('string').default('string'),
+    defaultValue: Joi.string().default(''),
+    valueOptions: Joi.array().length(0).allow(null).default(null),
+    inputType: Joi.string().valid('text', 'textarea').default('text'),
+    regex: Joi.string(),
+  }).required(),
+  commonSchema.keys({
+    type: Joi.string().valid('string').default('string'),
+    defaultValue: Joi.string().default(''),
+    valueOptions: Joi.array().items(Joi.object().keys({
+      text: stringOrTranslationsSchema.required(),
+      value: Joi.string().required().description('Will be passed through parser'),
+    })),
+    inputType: Joi.string().valid('select', 'radio').default('select'),
+  }).required(),
+  commonSchema.keys({
+    type: Joi.string().valid('number').required(),
+    defaultValue: Joi.number().default(0),
+    valueOptions: Joi.array().length(0).allow(null).default(null),
+    inputType: Joi.number().valid('text').default('text'),
+    lowerbound: Joi.number().description('If provided, type should be number or integer and provided value should not be lower than this value'),
+    upperbound: Joi.number().description('If provided, type should be number or integer and provided value should not be higher than this value'),
+  }).required(),
+  commonSchema.keys({
+    type: Joi.string().valid('number').required(),
+    defaultValue: Joi.number().default(0),
+    valueOptions: Joi.array().items(Joi.object().keys({
+      text: stringOrTranslationsSchema.required(),
+      value: Joi.number().required().description('Will be passed through parser'),
+    })),
+    inputType: Joi.string().valid('select', 'radio').default('select'),
+  }).required(),
+  commonSchema.keys({
+    type: Joi.string().valid('integer').required(),
+    defaultValue: Joi.number().integer().default(0),
+    valueOptions: Joi.array().length(0).allow(null).default(null),
+    inputType: Joi.number().valid('text').default('text'),
+    lowerbound: Joi.number().description('If provided, type should be number or integer and provided value should not be lower than this value'),
+    upperbound: Joi.number().description('If provided, type should be number or integer and provided value should not be higher than this value'),
+  }).required(),
+  commonSchema.keys({
+    type: Joi.string().valid('number').required(),
+    defaultValue: Joi.number().integer().default(0),
+    valueOptions: Joi.array().items(Joi.object().keys({
+      text: stringOrTranslationsSchema.required(),
+      value: Joi.number().integer().required().description('Will be passed through parser'),
+    })),
+    inputType: Joi.string().valid('select', 'radio').default('select'),
+  }).required(),
+  commonSchema.keys({
+    type: Joi.string().valid('boolean').required(),
+    defaultValue: Joi.boolean().default(false),
+    valueOptions: Joi.array().length(0).allow(null).default(null),
+    inputType: Joi.number().valid('switch', 'checkbox').default('checkbox'),
+  }).required(),
+  commonSchema.keys({
+    type: Joi.string().valid('boolean').required(),
+    defaultValue: Joi.boolean().default(0),
+    valueOptions: Joi.array().items(Joi.object().keys({
+      text: stringOrTranslationsSchema.required(),
+      value: Joi.boolean().required().description('Will be passed through parser'),
+    })),
+    inputType: Joi.string().valid('select', 'radio').default('select'),
+  }).required(),
+  commonSchema.keys({
+    inputType: Joi.string().valid('file', 'files').required(),
+  }).required(),
+);
+
+const schema = getBaseFieldConfigurationSchema(commonBaseFieldConfigurationSchema)
   .tag('baseFieldConfiguration')
   .description('Defines which data can be stored in form fields.');
 
-interface ValueOption {
-  // string for backwards compatibility, shown at all locales
+interface ValueOption<T extends BaseField> {
   text: StringOrTranslations;
-  value: BaseField;
+  value: T;
 }
 
-interface BaseFieldConfiguration {
+interface SharedBaseFieldConfiguration {
+  /**
+   * key used to store the field value
+   * @pattern: ^[a-z][a-zA-Z\d]*$
+   */
   key: string;
-  type: 'string' | 'boolean' | 'number' | 'integer';
+  /**
+   * name of the field to be shown in UI
+   */
   name: StringOrTranslations;
-  inputType: 'text' | 'textarea' | 'select' | 'radio' | 'switch' | 'checkbox' | 'file' | 'files';
-  defaultValue: string | boolean | number;
-  valueOptions: ValueOption[] | null;
-  regex?: string;
-  lowerbound?: number;
-  upperbound?: number;
+  /**
+   * show this field in UI only if value of another form field, identified by provided `key`, is
+   * equal to provided `value`
+   */
   showIf?: { key: string, value: string | boolean | number };
   prefix?: StringOrTranslations;
   suffix?: StringOrTranslations;
   hint?: StringOrTranslations;
 }
 
+type integer = number;
+
+type BaseFieldConfiguration = ({
+  type: 'string';
+  /**
+   * @default ""
+   */
+  defaultValue: string;
+  /**
+   * @default null
+   */
+  valueOptions: null | [];
+  /**
+   * @default "text"
+   */
+  inputType: 'text' | 'textarea';
+  regex?: string;
+} | {
+  type: 'string';
+  /**
+   * @default ""
+   */
+  defaultValue: string;
+  /**
+   * @minItems 2
+   */
+  valueOptions: ValueOption<string>[];
+  /**
+   * @default "select"
+   */
+  inputType: 'select' | 'radio';
+} | {
+  type: 'number';
+  /**
+   * @default 0
+   */
+  defaultValue: number;
+  /**
+   * @default null
+   */
+  valueOptions: null | [];
+  /**
+   * @default "text"
+   */
+  inputType: 'text';
+  lowerbound?: number;
+  upperbound?: number;
+} | {
+  type: 'number';
+  /**
+   * @default 0
+   */
+  defaultValue: number;
+  /**
+   * @minItems 2
+   */
+  valueOptions: ValueOption<number>[];
+  /**
+   * @default "select"
+   */
+  inputType: 'select' | 'radio';
+} | {
+  type: 'integer';
+  /**
+   * @default 0
+   */
+  defaultValue: integer;
+  /**
+   * @default null
+   */
+  valueOptions: null | [];
+  /**
+   * @default "text"
+   */
+  inputType: 'text';
+  lowerbound?: number;
+  upperbound?: number;
+} | {
+  type: 'integer';
+  /**
+   * @default 0
+   */
+  defaultValue: integer;
+  /**
+   * @minItems 2
+   */
+  valueOptions: ValueOption<integer>[];
+  inputType: 'select' | 'radio';
+} | {
+  type: 'boolean';
+  /**
+   * @default false
+   */
+  defaultValue: boolean;
+  /**
+   * @default null
+   */
+  valueOptions: null | [];
+  /**
+   * @default "checkbox"
+   */
+  inputType: 'switch' | 'checkbox';
+} | {
+  type: 'boolean';
+  /**
+   * @default false
+   */
+  defaultValue: boolean;
+  /**
+   * @minItems 2
+   */
+  valueOptions: ValueOption<boolean>[];
+  /**
+   * @default "select"
+   */
+  inputType: 'select' | 'radio';
+} | {
+  inputType: 'file' | 'files';
+}) & SharedBaseFieldConfiguration;
+
 export {
   schema,
   BaseFieldConfiguration,
+  getBaseFieldConfigurationSchema,
+  commonBaseFieldConfigurationSchema,
 };
