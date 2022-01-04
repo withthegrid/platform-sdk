3f07bc37079bc8763add2679beb71b96fcd4d3b1
Yaroslav Dobzhanskyi <merlyn.tgz@gmail.com>
2022-01-04 04:00:09 +0200

    chore: renamed limit to rowsPerPage

--- src/routes/suggestions/find.ts
+++ src/routes/suggestions/find.ts
@@ -5,8 +5,8 @@ interface Request {
   query: {
     search: string;
     type: string;
+    rowsPerPage: number;
     offset?: string;
-    limit?: number;
   };
 }
 
@@ -22,14 +22,24 @@ const controllerGeneratorOptions: ControllerGeneratorOptionsWithClient = {
   method: 'get',
   path: '/suggestions',
   query: Joi.object().keys({
-    search: Joi.string().required().example('%v1.1.0%'),
+    search: Joi.string().required().example('v1.1.0'),
     type: Joi.string().required().example('issue.title'),
-    offset: Joi.string().example('Installed v1.1.0-rc3'),
-    limit: Joi.number().example(100),
+    offset: Joi.string().example('Installed v1.1.0-rc3')
+      .description('To retrieve the next page, provide last item of the array'),
+    rowsPerPage: Joi.number()
+      .integer()
+      .min(1)
+      .max(500)
+      .default(50),
   }).default(),
   right: { environment: 'READ' },
   response: Joi.object().keys({
-    results: Joi.array().items(Joi.string()).required(),
+    results: Joi.array().items(Joi.string()).required()
+      .example([
+        'Installed v1.1.0-rc4',
+        'Installed v1.1.0-rc5',
+        'Installed v1.1.0-rc6',
+      ]),
   }).required(),
   description: 'Get suggestions for field of type, filtered by search',
 };
