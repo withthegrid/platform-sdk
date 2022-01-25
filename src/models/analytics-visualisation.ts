bbb6d1698121fb34487353860f135d9da6115244
KaterynaZavorotna <kateryna@withthegrid.com>
2022-01-25 15:57:41 +0200

    #771 - Model for map analytics widget

--- src/models/analytics-visualisation.ts
+++ src/models/analytics-visualisation.ts
@@ -1,21 +1,59 @@
 import Joi from 'joi';
 
-const schema = Joi.object().keys({
-  type: Joi.string().example('line').required(),
+const graphVisualisationSchema = Joi.object().keys({
+  type: Joi.string().example('line').valid('line', 'scatter', 'stacked-bar', 'clustered-bar').required(),
   colors: Joi.array().items(Joi.string().example('#202f0c')).required(),
   xAxisColumnIndex: Joi.number().integer().example(null).allow(null)
     .required(),
   forceOrdinal: Joi.boolean().example(false).required(),
-})
+  showAllXAxisLabels: Joi.boolean().example(false).default(false),
+});
+
+const mapVisualisationSchema = Joi.object().keys({
+  type: Joi.string().example('map').valid('map').required(),
+  latitudeColumnIndex: Joi.number().integer().example(0)
+    .required(),
+  longitudeColumnIndex: Joi.number().integer().example(0)
+    .required(),
+  colorColumnIndex: Joi.number().integer().example(0).allow(null)
+    .required(),
+  tooltipColumnIndex: Joi.number().integer().example(0).allow(null)
+    .required(),
+});
+
+const tableVisualisationSchema = Joi.object().keys({
+  type: Joi.string().example('table').valid('table').required(),
+});
+
+const schema = Joi.alternatives().try(
+  graphVisualisationSchema,
+  mapVisualisationSchema,
+  tableVisualisationSchema,
+)
   .description('An object describing the visualisation of the results of an analytics query.')
   .tag('analyticsVisualisation');
 
-interface AnalyticsVisualisation {
-  type: string;
+interface TableAnalyticsVisualisation {
+  type: 'table';
+}
+
+interface GraphVisualisation {
+  type: 'line' | 'scatter' | 'stacked-bar' | 'clustered-bar';
   colors: string[];
   xAxisColumnIndex: number | null;
   forceOrdinal: boolean;
+  showAllXAxisLabels: boolean;
 }
+
+interface MapsVisualisation {
+  type: 'map';
+  latitudeColumnIndex: number;
+  longitudeColumnIndex: number;
+  colorColumnIndex: number | null;
+  tooltipColumnIndex: number | null;
+}
+
+type AnalyticsVisualisation = TableAnalyticsVisualisation | GraphVisualisation | MapsVisualisation;
 
 export {
   schema,
