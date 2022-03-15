4ef43665bb2a211b92b452ab4200684b9c0ced58
Simon <simon@withthegrid.com>
2022-03-15 17:00:20 +0100

    feat: 944 automatic documentation WIP

--- src/models/string-or-translations.ts
+++ src/models/string-or-translations.ts
@@ -4,7 +4,9 @@ import { schema as translationsSchema, Translations } from './translations';
 const schema = Joi.alternatives(
   Joi.string().example('untranslated string'),
   translationsSchema,
-).tag('stringOrTranslations').meta({ className: 'stringOrTranslations' });
+)
+  .tag('stringOrTranslations')
+  .meta({ className: 'stringOrTranslations' });
 
 type StringOrTranslations = string | Translations;
 
