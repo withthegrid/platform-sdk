4dc084e18fba9f452ac414c81d15ad0c5119442a
Aurelien Arino <aurelien.arino@gmail.com>
2022-05-13 16:34:59 +0200

    chore(analytics-tables): add pluralization and match tests, no additional helpers

---
+++ test/analytics/analytics-tables.test.ts
@@ -0,0 +1,148 @@
+import { localesKeys, LocalesKeys } from '../../src/models/translations';
+import { AggregatedColumn, UnaggregatedColumn } from '../../src/models/analytics-query';
+import { getColumnPlaceholder } from '../../src/models/analytics/analytics-tables';
+
+describe('analytics-tables placeholders', () => {
+  describe('given the column type is undefined (unaggregated)', () => {
+    const unaggregatedColumn = (field: string): UnaggregatedColumn => ({
+      type: undefined,
+      field,
+    });
+
+    describe('when I ask for a field placeholder', () => {
+      it.each([
+        {
+          field: 'pinGroup.hashId',
+          expected: {
+            nl: 'Locatie:id',
+            en: 'Location:id',
+          },
+        },
+        {
+          field: 'pinGroup.fields.id',
+          expected: {
+            nl: 'Locatie:id',
+            en: 'Location:id',
+          },
+        },
+        {
+          field: 'measurement.value',
+          expected: {
+            nl: 'Meting:waarde',
+            en: 'Measurement:value',
+          },
+        },
+      ])('then it returns me a simple placeholder ($field)', ({ field, expected }) => {
+        // arrange
+        expect.assertions(localesKeys.length);
+        const column = unaggregatedColumn(field);
+
+        Object.entries(expected).forEach(([locale, expectedPlaceholder]) => {
+          // act
+          const placeholder = getColumnPlaceholder(column, locale as LocalesKeys);
+
+          // assert
+          expect(placeholder).toEqual(expectedPlaceholder);
+        });
+      });
+    });
+
+    describe('when I ask for a formField placeholder', () => {
+      it.each([
+        {
+          field: 'pin.deviceFields.devTyChannelFormField',
+          expected: {
+            nl: 'Poort:device formulierveld "devTyChannelFormField"',
+            en: 'Port:device form field "devTyChannelFormField"',
+          },
+        },
+      ])('then it returns me a form field placeholder ($field)', ({ field, expected }) => {
+        // arrange
+        expect.assertions(localesKeys.length);
+        const column = unaggregatedColumn(field);
+
+        Object.entries(expected).forEach(([locale, expectedPlaceholder]) => {
+          // act
+          const placeholder = getColumnPlaceholder(column, locale as LocalesKeys);
+
+          // assert
+          expect(placeholder).toEqual(expectedPlaceholder);
+        });
+      });
+    });
+  });
+
+  describe('given I ask for a column with a type (aggregated)', () => {
+    it.each([
+      {
+        name: 'any',
+        column: {
+          type: 'any',
+          field: 'pin.deviceFields.devTyChannelFormField',
+        },
+        expected: {
+          nl: 'Een(Poort:device formulierveld "devTyChannelFormField")',
+          en: 'Any(Port:device form field "devTyChannelFormField")',
+        },
+      },
+      {
+        name: 'count',
+        column: {
+          type: 'count',
+        },
+        expected: {
+          nl: 'Aantal', en: 'Count',
+        },
+      },
+      {
+        name: 'timegroup day measurement generated at',
+        column: {
+          type: 'timeGroup',
+          granularity: 'day',
+          field: 'measurement.generatedAt',
+        },
+        expected: {
+          nl: 'Dag(Meting:gemeten op)',
+          en: 'Day(Measurement:measured at)',
+        },
+      },
+      {
+        name: 'condition on min',
+        column: {
+          type: 'min',
+          field: 'measurement.value',
+          condition: {
+            type: 'or',
+            restrictions: [
+              {
+                comparison: '>',
+                left: 'measurement.value',
+                right: { value: '200' },
+              },
+            ],
+          },
+        },
+        expected: {
+          nl: 'Min*(Meting:waarde)',
+          en: 'Min*(Measurement:value)',
+        },
+      },
+    ])('then it returns me the placeholder ($name)', ({
+      column, expected,
+    }) => {
+      // arrange
+      expect.assertions(localesKeys.length);
+
+      Object.entries(expected).forEach(([locale, expectedPlaceholder]) => {
+        // act
+        const placeholder = getColumnPlaceholder(
+          column as AggregatedColumn,
+          locale as LocalesKeys,
+        );
+
+        // assert
+        expect(placeholder).toEqual(expectedPlaceholder);
+      });
+    });
+  });
+});
