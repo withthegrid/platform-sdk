2c2d3afb19d03cef1a1d62057677536a99967a99
KaterynaZavorotna <kateryna@withthegrid.com>
2022-05-19 10:05:07 +0200

    #884 - Added examples

--- src/routes/twofactorauthentication/validate-password-login.ts
+++ src/routes/twofactorauthentication/validate-password-login.ts
@@ -17,12 +17,12 @@ const controllerGeneratorOptions: ControllerGeneratorOptionsWithoutClientOrSuppl
   method: 'get',
   path: '/validate-password-login',
   query: Joi.object().keys({
-    email: Joi.string().required(),
-    password: Joi.string().required(),
+    email: Joi.string().required().example('test@test.com'),
+    password: Joi.string().required().example('iampassword'),
   }).required(),
   response: Joi.object().keys({
-    isCorrectPassword: Joi.boolean().required(),
-    hasTwoFAEnabled: Joi.boolean().required(),
+    isCorrectPassword: Joi.boolean().required().example(true),
+    hasTwoFAEnabled: Joi.boolean().required().example(true),
   }).required(),
   right: { },
 };
