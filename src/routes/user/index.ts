07dd53ebfc452d1fce5f3cfe4a86fce5d796509e
KaterynaZavorotna <kateryna@withthegrid.com>
2022-05-26 15:32:53 +0200

    #884 - Missed line fix

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -3,6 +3,7 @@ import * as deleteRoute from './delete';
 import * as find from './find';
 import * as get from './get';
 import * as update from './update';
+
 import Comms from '../../comms';
 
 import controllerGenerator, { Result } from '../../comms/controller';
