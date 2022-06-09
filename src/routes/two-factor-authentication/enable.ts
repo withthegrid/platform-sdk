06dfc9c4500fe29b07d53bd05b20b91753d61d2c
everhardt <rob@everhardt.nl>
2022-06-09 10:05:27 +0200

    fix: return JWT in 2fa.enable

--- src/routes/two-factor-authentication/enable.ts
+++ src/routes/two-factor-authentication/enable.ts
@@ -7,16 +7,21 @@ interface Request {
   };
 }
 
-type Response = void;
+interface Response {
+  jwt: string;
+}
 
 const controllerGeneratorOptions: ControllerGeneratorOptionsWithoutClientOrSupplier = {
   method: 'post',
   path: '/enable',
-  right: { },
+  right: {},
   body: Joi.object().keys({
     code: Joi.string().max(255).required().example('123456'),
   }),
-  description: 'Checks whether user entered correct code from authenticator app and enables 2FA for user',
+  response: Joi.object().keys({
+    jwt: Joi.string().required().example('a1234'),
+  }),
+  description: 'Checks whether user entered correct code from authenticator app and enables 2FA for user. The existing JWT cannot be used in the Authorization header anymore, instead the returned JWT must be used.',
 };
 
 export {
