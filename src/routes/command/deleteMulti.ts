aafb3a13653f7ebb5c2dd9c4e71e8a075a537486
Aurelien Arino <aurelien.arino@gmail.com>
2022-07-11 11:33:14 +0200

    chore(command.deleteMulti): fmt

--- src/routes/command/deleteMulti.ts
+++ src/routes/command/deleteMulti.ts
@@ -8,41 +8,48 @@ interface Request {
 }
 
 type ValidResponse = {
-	hashId: string
-	deleted: true
-}
+  hashId: string;
+  deleted: true;
+};
 
 type ErrorResponse = {
-	hashId: string
-	deleted: false
-	failureReason: string
-}
+  hashId: string;
+  deleted: false;
+  failureReason: string;
+};
 
 type Response = Array<ValidResponse | ErrorResponse>;
 
 const validResponseSchema = Joi.object().keys({
-	hashId: Joi.string().required().example('ga9741s'),
-	deleted: Joi.boolean().default(true).disallow(false).example(true),
+  hashId: Joi.string().required().example('ga9741s'),
+  deleted: Joi.boolean().default(true).disallow(false).example(true),
 });
 
 const errorResponseSchema = Joi.object().keys({
-	hashId: Joi.string().required().example('ga9741s'),
-	deleted: Joi.boolean().default(false).disallow(true).example(false),
-	failureReason: Joi.string().example('Device cannot be reached in time to cancel this command.'),
+  hashId: Joi.string().required().example('ga9741s'),
+  deleted: Joi.boolean().default(false).disallow(true).example(false),
+  failureReason: Joi.string().example(
+    'Device cannot be reached in time to cancel this command.',
+  ),
 });
 
 const controllerGeneratorOptions: ControllerGeneratorOptionsWithClientAndSupplier = {
   method: 'delete',
   path: '/',
-  body: Joi.object().keys({
-    hashIds: Joi.array().items(Joi.string()).required().example(['ga9741s']),
-  }).required(),
-  response: Joi.array().items(Joi.alternatives([
-		validResponseSchema,
-		errorResponseSchema,
-	])),
+  body: Joi.object()
+    .keys({
+      hashIds: Joi.array()
+        .items(Joi.string())
+        .required()
+        .example(['ga9741s']),
+    })
+    .required(),
+  response: Joi.array().items(
+    Joi.alternatives([validResponseSchema, errorResponseSchema]),
+  ),
   right: { environment: 'SENSORS', supplier: 'ENVIRONMENT_ADMIN' },
-  description: 'Delete multiple commands. Will return an array showing which commands were deleted or not.',
+  description:
+      'Delete multiple commands. Will return an array showing which commands were deleted or not.',
 };
 
 export {
