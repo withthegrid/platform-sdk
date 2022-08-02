fd0d99b60671c06f97f54226842e714ac6046a97
Simon <simon@withthegrid.com>
2022-08-02 13:53:27 +0200

    review comments

--- src/models/identity-provider.ts
+++ src/models/identity-provider.ts
@@ -11,7 +11,7 @@ const schema = (): Joi.ObjectSchema => Joi.object().keys({
   hashId: Joi.string().required().example('wasd2'),
   name: Joi.string().required(),
   url: Joi.string().required().example('https://login.microsoftonline.com/abvsd23fasdfasdf/v2.0/.well-known/openid-configuration'),
-  ssoClientId: Joi.string().required().example('ed055c9d-c3b9-449a-818e-5ffd18d49f98'),
+  ssoClientId: Joi.string().required().example('e39cb59e-3a74-48cc-a71d-77b8ef037ec9'),
 });
 
 export { schema, IdentityProvider };
