4aea2f6bd070c956105b739551d3028e7f60283a
srieding <32761158+srieding@users.noreply.github.com>
2022-08-02 16:17:40 +0200

    feat: login a user over sso part 1 (logging in) (#269)

---
+++ src/models/identity-provider.ts
@@ -0,0 +1,17 @@
+import Joi from 'joi';
+
+interface IdentityProvider {
+  hashId: string;
+  name: string;
+  url: string;
+  ssoClientId: string;
+}
+
+const schema = (): Joi.ObjectSchema => Joi.object().keys({
+  hashId: Joi.string().required().example('wasd2'),
+  name: Joi.string().required(),
+  url: Joi.string().required().example('https://login.microsoftonline.com/abvsd23fasdfasdf/v2.0/.well-known/openid-configuration'),
+  ssoClientId: Joi.string().required().example('e39cb59e-3a74-48cc-a71d-77b8ef037ec9'),
+});
+
+export { schema, IdentityProvider };
