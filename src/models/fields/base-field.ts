1ef325487fcf039599d7c43a8ec849279ac66f1c
Gabriel Padureanu <gabriel@withthegrid.com>
2022-11-16 16:38:27 +0100

    fix(fields): remove added alternative

--- src/models/fields/base-field.ts
+++ src/models/fields/base-field.ts
@@ -3,18 +3,11 @@ import Joi from 'joi';
 type BaseField = boolean | number | string | null | undefined;
 
 // strict(): do not do casting here, otherwise for example a string might end up as a number
-const baseFieldTypes: Joi.SchemaLikeWithoutArray[] = [
+const schema = Joi.alternatives().try(
   Joi.boolean().example(true).strict().required(),
   Joi.number().strict().required(),
   Joi.string().strict().allow('').required(),
-  Joi.any().valid(null).required()];
-
-const schema = Joi.alternatives().try(
-  ...baseFieldTypes,
-  Joi.object().keys({
-    value: Joi.alternatives().try(...baseFieldTypes).required(),
-    displayValue: Joi.string(),
-  }),
+  Joi.any().valid(null).required(),
 )
   .tag('baseField')
   .description('The values that can be stored in a form field');
