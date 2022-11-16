bec486886c43aad24b38b2148cbb9d11833e5a4c
Gabriel Padureanu <gabriel@withthegrid.com>
2022-11-16 01:06:52 +0100

    feat(form-fields): add hashId type

--- src/models/fields/base-field.ts
+++ src/models/fields/base-field.ts
@@ -3,11 +3,18 @@ import Joi from 'joi';
 type BaseField = boolean | number | string | null | undefined;
 
 // strict(): do not do casting here, otherwise for example a string might end up as a number
-const schema = Joi.alternatives().try(
+const baseFieldTypes: Joi.SchemaLikeWithoutArray[] = [
   Joi.boolean().example(true).strict().required(),
   Joi.number().strict().required(),
   Joi.string().strict().allow('').required(),
-  Joi.any().valid(null).required(),
+  Joi.any().valid(null).required()];
+
+const schema = Joi.alternatives().try(
+  ...baseFieldTypes,
+  Joi.object().keys({
+    value: Joi.alternatives().try(...baseFieldTypes).required(),
+    displayValue: Joi.string(),
+  }),
 )
   .tag('baseField')
   .description('The values that can be stored in a form field');
