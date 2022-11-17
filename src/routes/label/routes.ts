e845991ee76238e61764b092d64ca0a8f9a2192c
Aurelien Arino <aurelien.arino@gmail.com>
2022-11-17 12:02:49 +0100

    feat(label): add get endpoint

--- src/routes/label/routes.ts
+++ src/routes/label/routes.ts
@@ -2,10 +2,12 @@ import * as add from './add';
 import * as deleteRoute from './delete';
 import * as find from './find';
 import * as update from './update';
+import * as get from './get';
 
 export {
   add,
   deleteRoute as delete,
   find,
   update,
+  get,
 };
