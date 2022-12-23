4b7f2af074ad7f7be79c459c23903c37f9fe73d1
everhardt <rob@everhardt.nl>
2022-12-23 14:47:38 +0100

    chore: align with timeseries

--- src/routes/report/update.ts
+++ src/routes/report/update.ts
@@ -5,25 +5,6 @@ import {
   schema as fieldsToServerUpdateSchema, FieldsToServerUpdate,
 } from '../../models/fields/fields-to-server-update';
 
-interface NewMeasurement {
-  pinHashId: string;
-  quantityHashId: string;
-  generatedAt: Date;
-  orderOfMagnitude: number;
-  significand: number;
-  doNotCompareToThresholds?: boolean;
-}
-
-interface UpdatedMeasurement {
-  linkHashId: string;
-  generatedAt: Date;
-  orderOfMagnitude: number;
-  significand: number;
-  doNotCompareToThresholds?: boolean;
-}
-
-type ReportUpdate = UpdatedMeasurement | NewMeasurement;
-
 interface Request {
   params: {
     hashId: string;
@@ -31,7 +12,14 @@ interface Request {
   body: {
     generatedAt?: Date;
     fields?: FieldsToServerUpdate;
-    measurements?: ReportUpdate[];
+    measurements?: {
+      pinHashId: string;
+      quantityHashId: string;
+      generatedAt: Date;
+      orderOfMagnitude: number;
+      significand: number;
+      doNotCompareToThresholds?: boolean;
+    }[];
   };
 }
 
@@ -47,37 +35,20 @@ const controllerGeneratorOptions: ControllerGeneratorOptionsWithClient = {
   }).required(),
   body: Joi.object().keys({
     generatedAt: Joi.date().example('2019-12-31T15:23Z').description('The timestamp of the report itself. If not provided, the timestamp of the most recent measurement is taken. If no measurements are present, the existing generatedAt of the report is used.'),
-    measurements: Joi.array().items(Joi.alternatives().try(
-      Joi.object().keys({
-        linkHashId: Joi.string().required().example('a1b2c3'),
-        generatedAt: Joi.date().required().example('2019-12-31T15:23Z'),
-        orderOfMagnitude: Joi.number().integer().min(-128).max(127)
-          .required()
-          .example(-3)
-          .description('The measured value is significand * 10 ^ orderOfMagnitude. It has as many significant figures as the significand has (except when the significand is 0, then the number of significant figures is not defined)'),
-        significand: Joi.number().integer().min(-2147483648).max(2147483647)
-          .required()
-          .example(-1400)
-          .description('The measured value is significand * 10 ^ orderOfMagnitude. It has as many significant figures as the significand has (except when the significand is 0, then the number of significant figures is not defined)'),
-        doNotCompareToThresholds: Joi.boolean().default(false)
-          .example(true)
-          .description('Do not compare to thresholds'),
-      }),
-      Joi.object().keys({
-        pinHashId: Joi.string().required().example('e13d57'),
-        quantityHashId: Joi.string().required(),
-        generatedAt: Joi.date().required(),
-        orderOfMagnitude: Joi.number().integer().min(-128).max(127)
-          .required()
-          .description('The measured value is significand * 10 ^ orderOfMagnitude. It has as many significant figures as the significand has (except when the significand is 0, then the number of significant figures is not defined)'),
-        significand: Joi.number().integer().min(-2147483648).max(2147483647)
-          .required()
-          .description('The measured value is significand * 10 ^ orderOfMagnitude. It has as many significant figures as the significand has (except when the significand is 0, then the number of significant figures is not defined)'),
-        doNotCompareToThresholds: Joi.boolean().default(false)
-          .example(true)
-          .description('Do not compare to thresholds'),
-      }),
-    )),
+    measurements: Joi.array().items(Joi.object().keys({
+      pinHashId: Joi.string().required().example('e13d57'),
+      quantityHashId: Joi.string().required(),
+      generatedAt: Joi.date().required(),
+      orderOfMagnitude: Joi.number().integer().min(-128).max(127)
+        .required()
+        .description('The measured value is significand * 10 ^ orderOfMagnitude. It has as many significant figures as the significand has (except when the significand is 0, then the number of significant figures is not defined)'),
+      significand: Joi.number().integer().min(-2147483648).max(2147483647)
+        .required()
+        .description('The measured value is significand * 10 ^ orderOfMagnitude. It has as many significant figures as the significand has (except when the significand is 0, then the number of significant figures is not defined)'),
+      doNotCompareToThresholds: Joi.boolean().default(false)
+        .example(true)
+        .description('Do not compare to thresholds'),
+    })),
     fields: fieldsToServerUpdateSchema,
   }).required(),
   right: { environment: 'REPORTS' },
@@ -91,6 +62,4 @@ export {
   Request,
   Request as EffectiveRequest,
   Response,
-  NewMeasurement,
-  UpdatedMeasurement,
 };
