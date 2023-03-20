d41c3947edeece599509b8046f08b26ff72b8d31
Aurelien Arino <aurelien.arino@gmail.com>
2023-03-20 18:03:04 +0100

    comment

--- src/routes/chart/get-panels.ts
+++ src/routes/chart/get-panels.ts
@@ -12,6 +12,14 @@ type KindsHashIds = typeof kindsHashIds[number];
 const kindsResources = ['pinGroups', 'pins'] as const;
 type KindsResources = typeof kindsResources[number];
 
+// We make eveything Partial because having everything required but allowing an empty array does not
+// work. The reason is that coming from the consummer, having empty array(s) is valid, then the
+// query goes through axios, which strips the empty query arrays. Later, when the platform receives
+// the query and validated the requests, the requests does not validates against the schema anymore.
+// One alternative would be to add a paramSerializer to axios to force to produce a query param for
+// empty arrays. But doing so would annoys Joi because `array[0]` cannot be an empty string.
+// So the next best thing we can do is this, allowing empty queries, and anserwing with empty
+// responses
 type Request = {
   query: Partial<Record<KindsHashIds, Array<HashId>>>;
 }
@@ -41,8 +49,8 @@ const controllerGeneratorOptions: ControllerGeneratorOptionsWithClient = {
   method: 'get',
   path: '/panels',
   query: Joi.object().keys({
-    pinGroupHashIds: Joi.array().items(Joi.string()).max(30),
-    pinHashIds: Joi.array().items(Joi.string()).max(30),
+    pinGroupHashIds: Joi.array().items(Joi.string().example('dao97')).max(30),
+    pinHashIds: Joi.array().items(Joi.string().example('e13d57')).max(30),
   }).required(),
   right: { environment: 'READ' },
   response: (apiVersion: number): Joi.ObjectSchema => Joi.object().keys({
